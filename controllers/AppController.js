213dc344cdcde7b837693ecec032cf9d50029874
Olusegun Adeleke <oadeleke123@gmail.com>
2024-08-07 14:21:15 +0100

    add: node first api

---
+++ controllers/AppController.js
@@ -0,0 +1,32 @@
+const redisUtil = require('../utils/redis');
+const dbsUtil = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    try {
+      const redStatus = redisUtil.isAlive();
+      const dbStatus = dbsUtil.isAlive();
+      res.status(200).json({
+        redis: redStatus,
+        db: dbStatus,
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const userCount = dbsUtil.nbUsers();
+      const filesCount = dbsUtil.nbFiles();
+      res.status(200).json({
+        users: userCount,
+        files: filesCount,
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+module.exports = AppController;
